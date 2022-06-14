b1e5b6290c6c0c5750ee5174311b2f04731db9de
Chat Dep <42887325+chatsopond@users.noreply.github.com>
2022-06-15 00:26:08 +0700

    Initial Commit

---
+++ LMWN Coin 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LMWN Coin 2
+//
+//  Created by Chatsopon Deepateep on 15/6/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LMWN Coin 2/LMWN_Coin_2App.swift
@@ -0,0 +1,17 @@
+//
+//  LMWN_Coin_2App.swift
+//  LMWN Coin 2
+//
+//  Created by Chatsopon Deepateep on 15/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct LMWN_Coin_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
